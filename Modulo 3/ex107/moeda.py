f20da2aae1462ea5209b3ca776afb316cc8387d9
Thiago Maciel <fonterrada15@gmail.com>
2021-12-10 17:21:20 -0300

    Exercicios de módulos

---
+++ Modulo 3/ex107/moeda.py
@@ -0,0 +1,14 @@
+def aumentar(preco,taxa):
+    res = preco + (preco * taxa/100)
+    return res
+
+def diminuir(preco,taxa):
+    res = preco - (preco * taxa/100)
+
+def dobro(preco):
+    res = preco * 2
+    return res
+
+def metade(preco):
+    res = preco / 2
+    return res
